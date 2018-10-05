43f92f6f335f9e09746896b72396b085d3f0fc98
Matt Etress <mattetress@me.com>
2018-10-05 01:20:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,11 @@
 def line(deli_line)
   if deli_line.size == 0
     puts "The line is currently empty."
+  elsif deli_line.size > 0
+    line = []
+    deli_line.each_with_index do |name, i|
+      line.push("#{i + 1}. #{name}")
+    end
+    puts "The line is currently: #{line.join(" ")}"
   end
 end
